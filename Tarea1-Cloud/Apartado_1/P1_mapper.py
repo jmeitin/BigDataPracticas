94b40c523bcbe2b1f3dad893a01e7d720a5ee5da
Pablo Fernández <62569164+PabloFernandezAl01@users.noreply.github.com>
2022-10-04 15:41:50 +0200

    Apartado 1 - Tarea 1 Big Data

--- Tarea1-Cloud/Apartado_1/P1_mapper.py
+++ Tarea1-Cloud/Apartado_1/P1_mapper.py
@@ -1,10 +1,20 @@
 #!/usr/bin/python
 
+# python3 P1_mapper.py Project < ../../Datasets/input.txt | sort -k 1,1 -t $'\t' | python3 P1_reducer.py
+
 import sys
 import re
 
+wordToFind = sys.argv[1]
+
 for line in sys.stdin:
-    words = re.sub(r'\W+', ' ', line).split()
+
+    if line != '\n':
+
+        words = re.sub(r'\W+', ' ', line).split()
+        
+        for word in words:
+            if (word == wordToFind):
+                print(line, end='')
+                break
     
-    for word in words:
-        print(word.lower() + "\t1")
--- Tarea1-Cloud/Apartado_2/P2_mapper.py
+++ Tarea1-Cloud/Apartado_2/P2_mapper.py
@@ -4,7 +4,9 @@ import sys
 import re
 
 for line in sys.stdin:
-    words = re.sub(r'\W+', ' ', line).split()
+    words = re.sub(r'\W+', "GET ", line).split()
+
+    print(words)
     
     for word in words:
         print(word.lower() + "\t1")
