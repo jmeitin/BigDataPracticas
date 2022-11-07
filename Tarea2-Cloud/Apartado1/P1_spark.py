76d0c7c0341e3ac812959ec259af6038532f0b82
Pablo Fernández <pablfe20@ucm.es>
2022-11-07 14:44:27 +0100

    Modificacion ejercicio 1 Parte 2

--- Tarea2-Cloud/Apartado1/P1_spark.py
+++ Tarea2-Cloud/Apartado1/P1_spark.py
@@ -1,5 +1,6 @@
 from pyspark import SparkConf, SparkContext
 import sys
+import re
 
 # spark-submit P1_spark.py word file
 
@@ -9,4 +10,13 @@ sc = SparkContext(conf = conf)
 word = sys.argv[1]
 file = sys.argv[2]
 
-mobydickRDD = sc.textFile(file).filter(lambda line: word in line.lower()).saveAsTextFile("output.txt")
+def makeStringArray(string_list):
+    str1 = ""
+ 
+    for ele in string_list:
+        str1 += ele + " "
+ 
+    return str1
+
+mobydickRDD = sc.textFile(file).map(lambda line: re.sub(r'\W+', ' ', line).split())\
+    .filter(lambda line: line.count(word)).map(lambda line: makeStringArray(line)).saveAsTextFile("output")
